2f2594b962a8ed8e40e005abdc66ae2b09da4415
yongwon992 <yongwon992@naver.com>
2025-02-07 21:51:49 +0900

    Prebuild 전 변경사항 커밋

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,14 @@
-module.exports = {
-  presets: ["babel-preset-expo"],
-  plugins: [
-    ["module:react-native-dotenv", {
-      "moduleName": "@env",
-      "path": ".env",
-      "safe": true,
-      "allowUndefined": false
-    }]
-  ]
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": true,
+        "allowUndefined": false
+      }]
+    ]
+  };
 };
