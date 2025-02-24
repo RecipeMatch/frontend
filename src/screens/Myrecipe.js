d73949f25b23be2bffa3839212550cb39a7a5813
UjinChang9993 <dnwls0254@naver.com>
2025-02-24 22:14:01 +0900

    Add files via upload

---
+++ src/screens/Myrecipe.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Myrecipe = () => {
+  return (
+    <View>
+      <Text>Myrecipe</Text>
+    </View>
+  )
+}
+
+export default Myrecipe
